6d55d83048064dcf4983b19f93219571ab4f8ae5
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2021-06-05 12:28:54 -0400

    added images to the dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,29 +12,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            // val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            // toast.show()
-            val resultTextView: TextView = findViewById(R.id.textView)
-            val resultTextView2: TextView = findViewById(R.id.textView2)
-            resultTextView.text = rollDice().toString()
-            resultTextView2.text = rollDice().toString()
-        }
+        rollButton.setOnClickListener {rollDice()}
+        rollDice()
     }
 
-    private fun rollDice(): Int {
-        val dice = Dice(6)
-        return dice.roll()
-    }
-
-    /*
     private fun rollDice() {
         val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        when(dice.roll()) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+            else -> R.drawable.ic_launcher_foreground
+        }
     }
-    */
 }
 
 class Dice(private val numSides: Int) {
